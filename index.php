43808bd9781f7410f16727adb491cd8726e412f7
p r o c e s s o r <59508966+Proc3ssa@users.noreply.github.com>
2023-02-09 20:11:08 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+class IMAGES{
+
+    public $directory;
+    public $img_width;
+    public $img_height;
+
+    public function __construct($directory,$img_width,$img_height){
+        $this -> directory = $directory;
+        $this -> img_width = $img_width;
+        $this -> img_height = $img_height;
+    }
+
+    public function viewImg(){
+
+        $dir = scandir($this -> directory);
+
+        
+
+    }
+
+   
+
+} 
+
+    $images = new IMAGES(__DIR__, "150px", "150px");
+
+    $images -> viewImg();
+
+
+
+
+
+?>
+not completed!!
